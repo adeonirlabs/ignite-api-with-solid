2cb8e01bb6dff2b618413691e2090823f415b20c
Adeonir Kohl <adeonir@gmail.com>
2024-09-26 07:21:06 -0300

    Update tsconfig to handle imports without extensions

--- src/shared/services/database.ts
+++ src/shared/services/database.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-import { env } from '~/config/env.ts'
+import { env } from '~/config/env'
 
 export const prisma = new PrismaClient({
   log: env.NODE_ENV === 'development' ? ['query'] : [],
--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,15 @@ export default defineConfig({
   splitting: true,
   clean: true,
   outDir: 'dist',
+  format: ['esm'],
+  dts: true,
+  treeshake: true,
   outExtension: () => ({
     js: '.js',
   }),
+  esbuildOptions(options) {
+    options.alias = {
+      '~': './src',
+    }
+  },
 })
