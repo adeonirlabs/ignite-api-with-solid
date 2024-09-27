b3ecb7a54e264c8969816d7fd24ea4f01c73b9aa
Adeonir Kohl <adeonir@gmail.com>
2024-09-27 08:23:32 -0300

    Add authenticate usecase

---
+++ src/features/user/use-cases/authenticate.usecase.ts
@@ -0,0 +1,27 @@
+import { compare } from 'bcryptjs'
+
+import type { AuthenticateRequest } from '~/features/user/dtos/authenticate.dto'
+import type { UserRepository } from '~/repositories/interfaces/user-repository'
+import { UnauthorizedError } from '~/shared/errors/unauthorized.error'
+
+export class AuthenticateUseCase {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute(data: AuthenticateRequest) {
+    const { email, password } = data
+
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      throw new UnauthorizedError('Invalid credentials')
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    if (!passwordMatch) {
+      throw new UnauthorizedError('Invalid credentials')
+    }
+
+    return { user }
+  }
+}
