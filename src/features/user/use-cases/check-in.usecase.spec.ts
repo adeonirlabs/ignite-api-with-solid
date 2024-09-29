f6bdec3b735e1d7dc0a4f658e377968cb2d5fb90
Adeonir Kohl <adeonir@gmail.com>
2024-09-29 00:08:58 -0300

    Add test to check in a user in a gym

---
+++ src/features/user/use-cases/check-in.usecase.spec.ts
@@ -0,0 +1,23 @@
+import { beforeEach, describe, expect, it } from 'bun:test'
+import { CheckInUseCase } from '~/features/user/use-cases/check-in.usecase'
+
+import { InMemoryCheckInRepository } from '~/repositories/in-memory/check-in.repository'
+
+describe('Check-in Use Case', () => {
+  let checkInRepository: InMemoryCheckInRepository
+  let checkInUseCase: CheckInUseCase
+
+  beforeEach(() => {
+    checkInRepository = new InMemoryCheckInRepository()
+    checkInUseCase = new CheckInUseCase(checkInRepository)
+  })
+
+  it('should be able to check in', async () => {
+    const { checkIn } = await checkInUseCase.execute({
+      userId: 'user-id',
+      gymId: 'gym-id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
