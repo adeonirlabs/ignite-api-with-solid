c3181523a5cd89b78a7ee134a01c15a39d358c5f
Adeonir Kohl <adeonir@gmail.com>
2024-09-30 10:47:39 -0300

    Add gym dependency to check in usecase

--- src/features/user/use-cases/check-in.usecase.ts
+++ src/features/user/use-cases/check-in.usecase.ts
@@ -3,11 +3,22 @@ import type {
   CheckInResponse,
 } from '~/features/user/dtos/check-in.dto'
 import type { CheckInRepository } from '~/repositories/interfaces/check-in.interface'
+import type { GymRepository } from '~/repositories/interfaces/gym.interface'
+import { NotFoundError } from '~/shared/errors/not-found'
 
 export class CheckInUseCase {
-  constructor(private readonly checkInRepository: CheckInRepository) {}
+  constructor(
+    private readonly checkInRepository: CheckInRepository,
+    private readonly gymRepository: GymRepository
+  ) {}
 
   async execute(data: CheckInRequest): Promise<CheckInResponse> {
+    const gym = await this.gymRepository.findById(data.gymId)
+
+    if (!gym) {
+      throw new NotFoundError('Gym not found')
+    }
+
     const { userId, gymId } = data
 
     const alreadyCheckedIn = await this.checkInRepository.findByUserIdAtDate(
