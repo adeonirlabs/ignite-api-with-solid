ae64f262c419d9d6d0d980a964d79950ed3d3ba2
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 12:37:25 -0300

    Add check in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from 'fastify'
 
+import { checkInsRoutes } from '~/modules/check-in/routes'
 import { gymRoutes } from '~/modules/gym/routes'
 import { userRoutes } from '~/modules/user/routes'
 
 export async function routes(app: FastifyInstance) {
   app.register(gymRoutes)
   app.register(userRoutes)
+  app.register(checkInsRoutes)
 }
