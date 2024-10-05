47c2fd1dec123f088d8cd6e11a782644f3f93a6c
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 08:23:48 -0300

    Add e2e test of create user controller

--- src/features/user/controllers/create-user.controller.e2e-spec.ts
+++ src/features/user/controllers/create-user.controller.e2e-spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('should be able to create a user', async () => {
-  expect(true).toBe(true)
+import { app } from '~/app'
+
+describe('Create user controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.status).toBe(201)
+  })
 })
