5f0fa3d3e41a4e1c8c35ea4ca3bed9e8998e3ef6
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 13:00:53 -0300

    Add e2e test to validate a check in

---
+++ src/modules/check-in/controllers/validate.controller.e2e-spec.ts
@@ -0,0 +1,53 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '~/app'
+import { prisma } from '~/services/database'
+import { createAuthUser } from '~/utils/tests/create-auth-user'
+
+describe('Validate check in controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to validate a check in', async () => {
+    const { token } = await createAuthUser(app)
+
+    const user = await prisma.user.findFirstOrThrow()
+
+    const gym = await prisma.gym.create({
+      data: {
+        name: 'TypeScript Gym',
+        latitude: -29.297956,
+        longitude: -51.500644,
+      },
+    })
+
+    const checkIn = await prisma.checkIn.create({
+      data: {
+        gymId: gym.id,
+        userId: user.id,
+      },
+    })
+
+    const response = await request(app.server)
+      .patch(`/check-ins/${checkIn.id}/validate`)
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.status).toBe(204)
+
+    const updatedCheckIn = await prisma.checkIn.findUniqueOrThrow({
+      where: {
+        id: checkIn.id,
+      },
+    })
+
+    expect(updatedCheckIn.validatedAt).not.toBeNull()
+    expect(updatedCheckIn.validatedAt).toBeInstanceOf(Date)
+  })
+})
