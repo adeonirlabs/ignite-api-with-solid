625685d76e51030e5afe06e4eef46443ea6a603c
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 09:26:38 -0300

    Add nearbyt gym controller and route

--- src/features/gym/dtos/fetch-nearby-gym.dto.ts
+++ src/features/gym/dtos/fetch-nearby-gym.dto.ts
@@ -2,8 +2,12 @@ import type { Gym } from '@prisma/client'
 import { z } from 'zod'
 
 export const fetchNearbyGymSchema = z.object({
-  userLatitude: z.number(),
-  userLongitude: z.number(),
+  userLatitude: z.number().refine((value) => Math.abs(value) < 90, {
+    message: 'Latitude must be between -90 and 90',
+  }),
+  userLongitude: z.number().refine((value) => Math.abs(value) < 180, {
+    message: 'Longitude must be between -180 and 180',
+  }),
 })
 
 export type FetchNearbyGymRequest = z.infer<typeof fetchNearbyGymSchema>
--- src/features/gym/routes.ts
+++ src/features/gym/routes.ts
@@ -1,16 +1,19 @@
 import type { FastifyInstance } from 'fastify'
 
 import { CreateGymController } from '~/features/gym/controllers/create-gym.controller'
+import { FetchNearbyGymController } from '~/features/gym/controllers/fetch-nearby-gym.controller'
 import { SearchGymController } from '~/features/gym/controllers/search-gym.controller'
 import { verifyJwt } from '~/middlewares/verify-jwt'
 
 export async function gymRoutes(app: FastifyInstance) {
   const createGymController = new CreateGymController()
+  const fetchNearbyGymController = new FetchNearbyGymController()
   const searchGymController = new SearchGymController()
 
   app.addHook('onRequest', verifyJwt)
 
   // Private routes
   app.post('/gyms', createGymController.handle)
+  app.get('/gyms/nearby', fetchNearbyGymController.handle)
   app.get('/gyms/search', searchGymController.handle)
 }
