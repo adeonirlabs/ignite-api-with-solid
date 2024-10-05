69e32dcefe2f2d785e4e1467f3bfcd4627c6b3f9
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 11:52:13 -0300

    Add e2e test of nearby gym

---
+++ src/modules/gym/controllers/fetch-nearby.controller.e2e-spec.ts
@@ -0,0 +1,60 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '~/app'
+import { createAuthUser } from '~/utils/tests/create-auth-user'
+
+describe('Fetch Nearby Gyms controller', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    const { token } = await createAuthUser(app)
+
+    // Nearby gym
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'TypeScript Gym',
+        description: 'Some description',
+        phone: '123456',
+        latitude: -29.2974162,
+        longitude: -51.5009403,
+      })
+
+    // Distant gym
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'JavaScript Gym',
+        description: 'Some description',
+        phone: '123456',
+        latitude: -29.4400073,
+        longitude: -51.5051592,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .set('Authorization', `Bearer ${token}`)
+      .query({
+        latitude: -29.2974162,
+        longitude: -51.5009403,
+      })
+      .send()
+
+    expect(response.status).toBe(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        name: 'TypeScript Gym',
+      }),
+    ])
+  })
+})
