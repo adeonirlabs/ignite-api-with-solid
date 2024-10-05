3afd5a0095065e00472467b7b1fe70b97e2dbfab
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 08:19:48 -0300

    Add vitest config for e2e tests

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,12 +1,16 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
     environment: 'node',
+    env: {
+      BASE_URL: process.env.BASE_URL,
+    },
     include: ['src/**/*.e2e-spec.ts'],
     environmentMatchGlobs: [['src/features/*/controllers/**', 'prisma']],
   },
