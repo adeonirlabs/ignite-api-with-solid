83e13261818aa66cea7c8150811578b20e36c7da
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 08:52:22 -0300

    Update create gym dto

--- src/features/gym/dtos/create-gym.dto.ts
+++ src/features/gym/dtos/create-gym.dto.ts
@@ -5,8 +5,12 @@ export const createGymSchema = z.object({
   name: z.string(),
   description: z.string().nullable(),
   phone: z.string().nullable(),
-  latitude: z.number(),
-  longitude: z.number(),
+  latitude: z.number().refine((value) => Math.abs(value) < 90, {
+    message: 'Latitude must be between -90 and 90',
+  }),
+  longitude: z.number().refine((value) => Math.abs(value) < 180, {
+    message: 'Longitude must be between -180 and 180',
+  }),
 })
 
 export type CreateGymRequest = z.infer<typeof createGymSchema>
