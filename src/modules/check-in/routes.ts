34ddc98d87e99efebcc4c97c100291ea73014c38
Adeonir Kohl <adeonir@gmail.com>
2024-10-05 10:31:20 -0300

    Add check in routes

--- src/modules/check-in/routes.ts
+++ src/modules/check-in/routes.ts
@@ -1,9 +1,22 @@
 import type { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '~/middlewares/verify-jwt'
+import { CreateCheckInController } from '~/modules/check-in/controllers/create.controller'
+import { FetchHistoryController } from '~/modules/check-in/controllers/fetch-history.controller'
+import { GetMetricsController } from '~/modules/check-in/controllers/get-metrics.controller'
+import { ValidateCheckInController } from '~/modules/check-in/controllers/validate.controller'
 
-export async function gymRoutes(app: FastifyInstance) {
+export async function checkInsRoutes(app: FastifyInstance) {
+  const createCheckInController = new CreateCheckInController()
+  const validateCheckInController = new ValidateCheckInController()
+  const fetchHistoryController = new FetchHistoryController()
+  const getMetricsController = new GetMetricsController()
+
   app.addHook('onRequest', verifyJwt)
 
   // Private routes
+  app.post('/gyms/:gymId/check-ins', createCheckInController.handle)
+  app.get('/check-ins/history', fetchHistoryController.handle)
+  app.get('/check-ins/metrics', getMetricsController.handle)
+  app.patch('/check-ins/:checkInId/validate', validateCheckInController.handle)
 }
