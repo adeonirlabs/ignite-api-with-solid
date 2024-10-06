f408c048f21b9b3317c22b935f29134df3f0f8e6
Adeonir Kohl <adeonir@gmail.com>
2024-10-06 07:48:08 -0300

    Add refresh token controller

--- src/modules/user/routes.ts
+++ src/modules/user/routes.ts
@@ -2,17 +2,20 @@ import type { FastifyInstance } from 'fastify'
 
 import { verifyJwt } from '~/middlewares/verify-jwt'
 import { AuthenticateController } from '~/modules/user/controllers/authenticate.controller'
+import { CreateUserController } from '~/modules/user/controllers/create.controller'
 import { ProfileController } from '~/modules/user/controllers/profile.controller'
-import { CreateUserController } from './controllers/create.controller'
+import { RefreshController } from '~/modules/user/controllers/refresh.controller'
 
 export async function userRoutes(app: FastifyInstance) {
-  const createUserController = new CreateUserController()
   const authenticateController = new AuthenticateController()
+  const createUserController = new CreateUserController()
   const profileController = new ProfileController()
+  const refreshController = new RefreshController()
 
   // Public routes
   app.post('/users', createUserController.handle)
   app.post('/sessions', authenticateController.handle)
+  app.patch('/refresh', refreshController.handle)
 
   // Private routes
   app.get('/profile', { onRequest: [verifyJwt] }, profileController.show)
