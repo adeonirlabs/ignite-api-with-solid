8ddad6e273d797af5e4cf403af85a8f5660900fb
Adeonir Kohl <adeonir@gmail.com>
2024-10-06 07:31:09 -0300

    Add refresh token to authenticate controller

--- src/modules/user/controllers/authenticate.controller.ts
+++ src/modules/user/controllers/authenticate.controller.ts
@@ -22,7 +22,25 @@ export class AuthenticateController {
         }
       )
 
-      return reply.status(200).send({ token })
+      const refreshToken = await reply.jwtSign(
+        {},
+        {
+          sign: {
+            sub: user.id,
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return reply
+        .status(200)
+        .setCookie('refreshToken', refreshToken, {
+          path: '/',
+          secure: true,
+          sameSite: true,
+          httpOnly: true,
+        })
+        .send({ token })
     } catch (error) {
       if (error instanceof UnauthorizedError) {
         return reply.status(401).send({ message: error.message })
